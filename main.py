163c1f646d7ed3db05c857577604f1c949acdf72
elohim <elohim@MacBook-Air-de-elohim.local>
2025-04-14 15:44:58 -0300

    primeiro upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
